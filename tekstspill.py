456851b53655a57314bedc5b669297f5dc968f1d
GetNetIsBad <38994664+GetNetIsBad@users.noreply.github.com>
2024-09-22 19:23:01 +0200

    sols

---
+++ tekstspill.py
@@ -0,0 +1,30 @@
+pos = 1
+
+print("start")
+print("forklaring")
+
+for i in range:
+
+    
+
+    if pos == 1:
+        print("startrom")
+    elif pos == 1 and map == True:
+        print("map/startrom")
+
+    elif pos == 2:
+        print("startrom")
+    elif pos == 2 and map == True:
+        print("map/startrom")
+
+    elif pos == 3:
+        print("startrom")
+    elif pos == 3 and map == True:
+        print("map/startrom")
+
+    elif pos == 4:
+        print("startrom")
+    elif pos == 4 and map == True:
+        print("map/startrom")
+
+
